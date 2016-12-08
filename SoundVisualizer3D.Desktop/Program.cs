9711c11ebbec5175569eff5142f6f008d911f08d
Philip Schneider <philip.schneider@tdmsystems.com>
2016-12-08 21:41:34 +0100

    Added Simple MonoGame Desktop Projekt.

---
+++ SoundVisualizer3D.Desktop/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SoundVisualizer3D.Desktop
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (MonoGame monoGame = new MonoGame())
+            {
+                monoGame.Run();
+            }
+        }
+    }
+}
